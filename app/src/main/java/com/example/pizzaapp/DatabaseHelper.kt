5a3b80d9d79a8061ee5a00bbd4eb87786998e01b
damar <damaradii.nugroho@gmail.com>
2023-11-15 17:00:07 +0700

    pizza commit

--- app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
+++ app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
@@ -1,12 +1,15 @@
 package com.example.pizzaapp
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Toast
 import com.google.gson.internal.bind.DateTypeAdapter
 
-class DatabaseHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+class DatabaseHelper(var context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
 
     companion object{
         private val DATABASE_NAME = "pizza"
@@ -64,4 +67,53 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,n
             return false
     }
 
+    fun addAccount (email:String, name:String, level:String, password:String) {
+        val db = this.readableDatabase
+
+        val values = ContentValues()
+        values.put(COLUMN_EMAIL, email)
+        values.put(COLUMN_NAME, name)
+        values.put(COLUMN_LEVEL, level)
+        values.put(COLUMN_PASSWORD, password)
+
+       val result = db.insert(TABLE_ACCOUNT, null, values)
+        //show massage
+        if (result == (0).toLong()) {
+            Toast.makeText(context, "Register Failed", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(context, "Register Success, " + "Please login using your new account", Toast.LENGTH_SHORT).show()
+        }
+       db.insert(TABLE_ACCOUNT, null, values)
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun checkData(email:String):String {
+        val columns = arrayOf(COLUMN_NAME)
+        val db = this.readableDatabase
+        //selection criteria
+        val selection = "$COLUMN_EMAIL = ?"
+        //selection argument
+        val selectionArgs = arrayOf(email)
+        var name: String = ""
+
+        val cursor = db.query(
+            TABLE_ACCOUNT, //tabble query
+            columns, //columns returns
+            selection, //columns for where clause
+            selectionArgs, //the values for the where clause
+            null,
+            null,
+            null
+        )
+
+        if (cursor.moveToFirst()) {
+            name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
+        }
+
+        cursor.close()
+        db.close()
+        return name
+    }
 }
